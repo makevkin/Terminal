33ed5ebc0c3a993c067d8aab01bc31d7099d5b12
makevkin <129867845+makevkin@users.noreply.github.com>
2023-07-23 19:42:51 +0400

    Update lesson_1.sh

--- lesson_1.sh
+++ lesson_1.sh
@@ -2,7 +2,7 @@
 	cd folder
 	mkdir dir1 dir2 dir3
 	cd dir2
-touch[D[D[	touch 1.txt 2.txt 3.txt 1.json 2.json
+	touch 1.txt 2.txt 3.txt 1.json 2.json
 	mkdir dir11 dir 22 dir 33
 	ls -la
 	mv dir2/{1.json, 2.json} dir1
